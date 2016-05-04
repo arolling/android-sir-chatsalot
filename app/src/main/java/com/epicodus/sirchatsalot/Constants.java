712c4820b9264f42891260c576b7bc1c220bc248
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-05-04 14:04:27 -0700

    Set up initial files and build.gradle etc.

---
+++ app/src/main/java/com/epicodus/sirchatsalot/Constants.java
@@ -0,0 +1,7 @@
+package com.epicodus.sirchatsalot;
+
+/**
+ * Created by Guest on 5/4/16.
+ */
+public class Constants {
+}
---
+++ app/src/main/java/com/epicodus/sirchatsalot/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.sirchatsalot.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.sirchatsalot.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
