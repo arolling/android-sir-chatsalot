4a9e49c3f4d9eb9dc83d5f685781f0b995776e32
Abby Rolling and Daren Schaad <darenschaad@gmail.com>
2016-05-04 15:15:31 -0700

    Create new user logic for create account activity

--- app/src/main/java/com/epicodus/sirchatsalot/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/sirchatsalot/ui/CreateAccountActivity.java
@@ -1,15 +1,78 @@
 package com.epicodus.sirchatsalot.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.epicodus.sirchatsalot.Constants;
 import com.epicodus.sirchatsalot.R;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = CreateAccountActivity.class.getSimpleName();
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.loginTextView:
+                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.createUserButton:
+                createNewUser();
+            default:
+                break;
+        }
+    }
+
+    public void createNewUser(){
+        final String name = mNameEditText.getText().toString();
+        final String email = mEmailEditText.getText().toString();
+        final String password = mPasswordEditText.getText().toString();
+        final String confirmPassword = mConfirmPasswordEditText.getText().toString();
+
+        if(password.equals(confirmPassword)){
+            mFirebaseRef.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+                @Override
+                public void onSuccess(Map<String, Object> result){
+
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError){
+                    Log.d(TAG, "error occurred " + firebaseError);
+                }
+            });
+        } else {
+            mPasswordEditText.setError("Your second password entry differed from the first");
+        }
     }
 }
