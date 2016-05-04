863135a2b3b48b2da6d4be962ca96efc1e2aba74
Abby Rolling and Daren Schaad <darenschaad@gmail.com>
2016-05-04 15:33:45 -0700

    Add user login functionality

---
+++ app/src/main/java/com/epicodus/sirchatsalot/models/Conversation.java
@@ -0,0 +1,7 @@
+package com.epicodus.sirchatsalot.models;
+
+/**
+ * Created by Guest on 5/4/16.
+ */
+public class Conversation {
+}
--- app/src/main/java/com/epicodus/sirchatsalot/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/sirchatsalot/ui/LoginActivity.java
@@ -1,20 +1,35 @@
 package com.epicodus.sirchatsalot.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.epicodus.sirchatsalot.Constants;
 import com.epicodus.sirchatsalot.R;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = LoginActivity.class.getSimpleName();
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
     @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    private Firebase mFirebaseRef;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+        mPasswordLoginButton.setOnClickListener(this);
     }
 
     @Override
@@ -32,8 +52,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.passwordLoginButton:
+                loginWithPassword();
             default:
                 break;
         }
     }
+
+    public void loginWithPassword() {
+        String email = mEmailEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+        } else if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+        } else {
+            mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler(){
+                @Override
+                public void onAuthenticated(AuthData authData){
+                    if(authData != null){
+                        String userUid = authData.getUid();
+                        mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                        String userInfo = authData.toString();
+                        Log.d(TAG, "Currently logged in: " + userInfo);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                }
+
+                @Override
+                public void onAuthenticationError(FirebaseError firebaseError) {
+                    switch (firebaseError.getCode()) {
+                        case FirebaseError.INVALID_EMAIL:
+                        case FirebaseError.USER_DOES_NOT_EXIST:
+                            mEmailEditText.setError("Please check that you entered your email correctly");
+                            break;
+                        case FirebaseError.INVALID_PASSWORD:
+                            mEmailEditText.setError(firebaseError.getMessage());
+                            break;
+                        case FirebaseError.NETWORK_ERROR:
+                            showErrorToast("There was a problem with the network connection");
+                            break;
+                        default:
+                            showErrorToast(firebaseError.toString());
+                    }
+                }
+
+            });
+        }
+    }
+
+    private void showErrorToast(String message) {
+        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+
+
 }
