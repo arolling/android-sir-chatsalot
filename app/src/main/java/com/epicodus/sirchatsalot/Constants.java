18eeea1f27c7795b3792598b2d2e1caaa19cad2d
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-05-04 14:56:05 -0700

    create layouts for login and create account activities

--- app/src/main/java/com/epicodus/sirchatsalot/Constants.java
+++ app/src/main/java/com/epicodus/sirchatsalot/Constants.java
@@ -1,7 +1,13 @@
 package com.epicodus.sirchatsalot;
 
+
 /**
  * Created by Guest on 5/4/16.
  */
 public class Constants {
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String KEY_UID = "uid";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+
 }
